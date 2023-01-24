06637767570ccdfe170b4ad776217d857cb26dc3
Tatiiics <tatiianacs@gmail.com>
2023-01-24 16:26:39 +0100

    Creacion entidad empleados

---
+++ src/main/java/com/springhibernate/springHibernate/data/entity/EmpleadosModel.java
@@ -0,0 +1,27 @@
+package com.springhibernate.springHibernate.data.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table (name="Empleados")
+
+public class EmpleadosModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // autoincremento del id
+    private int id_empleado;
+    @Column(nullable = false) //Para que no sea null el valor nombre
+    private String nombre;
+
+    private String apellidos;
+    @Column(nullable = false)
+    private String dni;
+    @Column(nullable = false)
+    private int salario;
+
+}
