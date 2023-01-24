098383870784865f37754df877c0ddc4408defa4
LesAileen <les96@hotmail.es>
2023-01-24 16:43:21 +0100

    Creacion entidad Departamentos

---
+++ src/main/java/com/springhibernate/springHibernate/data/entity/DepartamentosModel.java
@@ -0,0 +1,22 @@
+package com.springhibernate.springHibernate.data.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name="Departamentos")
+
+public class DepartamentosModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // autoincremento del id
+    private int codigo;
+    @Column(nullable = false)
+    private String nombre;
+    private int presupuesto;
+    private int gastos;
+}
