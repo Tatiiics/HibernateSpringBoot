261b74f7a76ce36f39341821a61d7d7a57e76ac0
Tatiiics <tatiianacs@gmail.com>
2023-01-24 17:07:35 +0100

    Creacion entidad Proyectos

---
+++ src/main/java/com/springhibernate/springHibernate/data/entity/ProyectosModel.java
@@ -0,0 +1,20 @@
+package com.springhibernate.springHibernate.data.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name="Proyectos")
+public class ProyectosModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // autoincremento del id
+    private int idProyecto;
+    @Column(nullable = false)
+    private String nombre;
+    private String presupuesto;
+}
